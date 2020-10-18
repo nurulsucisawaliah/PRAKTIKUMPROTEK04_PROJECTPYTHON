e9b73b3a3b5b4e705264f0047867ba44b8453682
nurulsucisawaliah <nurulsucisawaliah_26@student.und.ac.id>
2020-10-18 23:39:05 +0800

    Create latihan01.py

---
+++ LATIHAN/latihan01/latihan01.py
@@ -0,0 +1,11 @@
+#menghitung total tarif sewa mobil
+#12 jam pertama = 200.000jam
+#jam berikutnya = 10.000/jam
+#penyewaan darri jam 06.30-23.50(dihari yang sama)
+tarif1 = 200000
+tarif2 = 10000
+jamke2 = 5
+menitke2 = 50
+tarifmenitke2 = ((tarif2/60)*50)
+totaltarif = tarif1+(tarif2*5)+tarifmenitke2
+print(totaltarif)
