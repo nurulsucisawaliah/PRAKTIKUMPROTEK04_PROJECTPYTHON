d7f0c9a5fe502b1dde46a137617251e392406692
nurulsucisawaliah <nurulsucisawaliah_26@student.und.ac.id>
2020-10-18 23:39:23 +0800

    Create latihan04.py

---
+++ LATIHAN/latihan04/latihan04.py
@@ -0,0 +1,9 @@
+#menentukan pukul berapa pak Amir sampai di kota C
+jarak1 = 125
+kecepatan1 = 62
+jarak2 = 256
+kecepatan2 = 70
+waktuBerangkat = 06.00
+waktuIstirahat = 0.45
+sampaikotac = waktuBerangkat+waktuIstirahat+jarak1//kecepatan1+jarak2//kecepatan1
+print(sampaikotac)
