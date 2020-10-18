20281716ec5fd2bc3415c1c47f724fd1a40022a4
nurulsucisawaliah <nurulsucisawaliah_26@student.und.ac.id>
2020-10-18 23:39:12 +0800

    Create latihan02.py

---
+++ LATIHAN/latihan02/latihan02.py
@@ -0,0 +1,5 @@
+#hitung jumlah bensin yang diperlukan dari kota a ke kota b
+kapasitastangki = 50lt
+literbensindarikotaakeb = 66.25lt
+bensindiperlukan = literbensindarikotaakeb/kapasitastangki
+print(bensindiperlukan)
