cfa12356646382476ed5f4dfe2ad17a226918e96
nurulsucisawaliah <nurulsucisawaliah_26@student.und.ac.id>
2020-10-18 23:39:27 +0800

    Create latihan05.py

---
+++ LATIHAN/latihan05/latihan05.py
@@ -0,0 +1,8 @@
+#membuat program diagram jumlah mahasiswa PTIK UNS 2020
+
+mahasiswalakilaki = 57
+mahasiswaperempuan = 27
+diagram1 = '*'*mahasiswalakilaki
+print(diagram1)
+diagram2 ='*'*mahasiswaperempuan
+print(diagram2)
